40390f9fd5ab7704d66350930ae4da70e4c7110f
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-25 05:40:25 +0530

    kd

---
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Declare a variable 
+            string myfriends;
+            myfriends = "Pritish";
+            Console.WriteLine(myfriends);
+            myfriends = "Sagarika";
+            Console.WriteLine(myfriends);
+        }
+    }
+}
---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+//USER INPUT AND STORE
+string userinput=Console.ReadLine();
+Console.WriteLine(userinput);
+Console.ReadKey();
+
