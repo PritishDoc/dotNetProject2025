a6e2754c7c19d009b69304445ab0925abfed58f4
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-26 22:19:01 +0530

    summary

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Pritish");
+
+
+int myNum = 0;
+int mynum2 = 0;
+
+//Taking input for 1st num
+Console.WriteLine("Enter a whole num: ");
+
+
+string userInput = Console.ReadLine();
+myNum = int.Parse(userInput);
+
+Console.WriteLine("Enter the 2nd whole num: ");
+userInput = Console.ReadLine();
+mynum2 = int.Parse(userInput);
+
+int sum = myNum + mynum2;
+Console.WriteLine("The result of "+myNum+"is"+sum);
+
+
+
+
+//String interpolation
+Console.WriteLine($"The result of {myNum}and {mynum2} is {sum}");//less error prone  
+Console.ReadKey();
